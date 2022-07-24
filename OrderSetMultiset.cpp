4b41deb29be2bd1eb64bca2a2363cc99aa4b44fb
akhandagrwl-29 <coding.akhandagarwal6174@gmail.com>
2022-07-24 17:11:24 +0530

    Create OrderSetMultiset.cpp

---
+++ OrderSetMultiset.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define int             long long
+
+//for set
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update > new_s;
+//for multiset
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update > new_ms;
+//order_of_key(val): returns the number of values less than val (or position of val- 0 based)
+//find_by_order(k): returns an iterator to the kth largest element (0-based)
+
+
+int32_t main() {
+	new_s s;
+}
