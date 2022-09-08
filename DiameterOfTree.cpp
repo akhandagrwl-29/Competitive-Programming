12ea91726c0def5d45dd2b0a6d1a9880907b122c
akhandagrwl-29 <coding.akhandagarwal6174@gmail.com>
2022-09-08 07:58:59 +0530

    Update DiameterOfTree.cpp

--- DiameterOfTree.cpp
+++ DiameterOfTree.cpp
@@ -4,22 +4,22 @@ class Diameter{
 public:
     Diameter(vector<vector<int>> &g){
         pair<int  , int > a =  dfs(1 , 0  , g) ;
-        pair<int  , int > b =  dfs(a.ss , 0  , g) ;
-        ends  = {a.ss , b.ss} ;
-        len =  b.ff -1   ;
+        pair<int  , int > b =  dfs(a.second , 0  , g) ;
+        ends  = {a.second , b.second} ;
+        len =  b.first -1   ;
     }
 
     pair<int , int>dfs(int v , int u , vector<vector<int>> &g){
-        pair<int , int>  num   =  MP(0 , v) ;
+        pair<int , int>  num   =  make_pair(0 , v) ;
         for(auto &x : g[v]){
             if(x != u){
                 pair<int ,int> a = dfs(x , v , g) ;
-                if(a.ff > num.ff){
+                if(a.first > num.first){
                     num  =  a   ;
                 }
             }
         }
-        num.ff++  ;
+        num.first++  ;
         return num ; 
     }
 
